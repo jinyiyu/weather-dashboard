bddb80ad0836861689d6aa2e573978095fe1e695
Jinyi Yu <yujinyiicxk@gmail.com>
2022-05-30 18:40:04 +0100

    added main section layout and a bit comments on js

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,1 +1,9 @@
 console.log("hi");
+
+//when onload:
+// - banner section and serach form will always there
+
+//search history
+// - if has in LS then render the city name one by one for the top 8 most resent search
+// - if not render massage : no previous search history.
+// - add clear all searches button
